51052826955b9e7472244fa0cfefcd0faea68f4b
Claudio Ballardini <claudio.ballardini01@gmail.com>
2023-08-09 18:56:51 +0200

    Update production environment variables

--- small_claim_form/small_claim_form/form_b/src/environments/environment.ts
+++ small_claim_form/small_claim_form/form_b/src/environments/environment.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+  EXTERNAL_URI: "https://yellow-coast-0c9cc7403.2.azurestaticapps.net",
+  INTERNAL_URI: "https://yellow-coast-0c9cc7403.2.azurestaticapps.net",
+};
